7c1106e5c98589b5de673da4111e437d6d67507c
jiaxinggao <1357782551@qq.com>
2018-01-31 17:35:20 +0800

    高佳兴的提交

--- src/containers/MyMusic/MyMusic.js
+++ src/containers/MyMusic/MyMusic.js
@@ -1,76 +1,88 @@
 import React from 'react';
 import './index.less';
+import {Link} from 'react-router-dom';
+
 import EstablishList from "./EstablishList";
 import Shade from "./Shade";
 import MHeader from "../../components/MHeader/MHeader";
+
+
 export default class MyMusic extends React.Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
-      isShow:false,
-      isPopUp:false
+    this.state = {
+      isShow: false,
+      isPopUp: false
     }
   }
 
-  clickShow=()=>{
-    this.setState({isShow:!this.state.isShow})
-  };
-  clickPopUp=(e)=>{
+  clickShow = (e) => {
+    this.setState({isShow: !this.state.isShow});
     e.stopPropagation();
-    this.setState({isPopUp:!this.state.isPopUp});
-    console.log(1);
+  };
+  clickPopUp = (e) => {
+    e.stopPropagation();
+    this.setState({isPopUp: !this.state.isPopUp});
   };
 
 
-
-  render(){
-
+  render() {
     return (
-        <div className='mymusic'>
-          <MHeader>
-            <i className="iconfont icon-yunzhanghu"></i>
-            <div className='headerCenter'>
-              我的音乐
-            </div>
-          </MHeader>
-          <div className='content'>
-            <ul className='select-list'>
-              <li className='clearfix'>
-                <i className='front'></i>
+      <div className='mymusic'>
+        <MHeader>
+          <i className="iconfont icon-yunzhanghu"></i>
+          <div className='headerCenter'>
+            我的音乐
+          </div>
+        </MHeader>
+
+
+        <div className='content'>
+          <ul className='select-list'>
+            <li className='clearfix'>
+              <Link to='/'>
+                <i className='iconfont icon-yinyue front'></i>
                 <p>本地音乐</p>
                 <span>9</span>
-                <i className='back'></i>
-              </li>
-              <li>
-                <i className='front'></i>
+                <i className='back iconfont icon-fanhui2'></i>
+              </Link>
+            </li>
+            <li>
+              <Link to='/'>
+                <i className='iconfont icon-bofang2 front '></i>
                 <p>最近播放</p>
                 <span>100</span>
-                <i className='back'></i>
-              </li>
-              <li>
-                <i className='front'></i>
+                <i className='back iconfont icon-fanhui2'></i>
+              </Link>
+            </li>
+            <li>
+              <Link to='/'>
+                <i className='front iconfont icon-diantai'></i>
                 <p>我的电台</p>
                 <span>7</span>
-                <i className='back'></i>
-              </li>
-              <li className='last'>
-                <i className='front'></i>
+                <i className='back iconfont icon-fanhui2'></i>
+              </Link>
+            </li>
+            <li className='last'>
+              <Link to='/'>
+                <i className='front iconfont icon-shoucang'></i>
                 <p>我的收藏</p>
-                <span>333</span>
-                <i className='back'></i>
-              </li>
-            </ul>
-            <div className='song-list'>
-              <div className='song-text' onClick={this.clickShow}>
-              <i className='forwards'></i>
+                <span>33</span>
+                <i className='back iconfont icon-fanhui2'></i>
+              </Link>
+            </li>
+          </ul>
+          <div className='song-list'>
+            <div className='song-text' onClick={this.clickShow}>
+              <i className='forwards iconfont icon-fanhui2'></i>
               <span>我创建的歌单(111)</span>
-              <i className='backwards' onClick={this.clickPopUp}></i>
-              {this.state.isShow?<EstablishList/>:null}
-              {this.state.isPopUp?<Shade/>:null}
-            </div>
+              <i className='backwards iconfont icon-more' onClick={this.clickPopUp}></i>
+              {this.state.isShow ? <EstablishList/> : null}
             </div>
           </div>
+          {this.state.isPopUp ? <Shade/> : null}
         </div>
+      </div>
     )
   }
 }
