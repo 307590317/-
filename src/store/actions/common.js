84a7b373e878bcd94f30cef758294dacdefa13e4
307590317 <307590317@qq.com>
2018-02-01 21:45:39 +0800

    最新上传

---
+++ src/store/actions/common.js
@@ -0,0 +1,20 @@
+import * as Types from "../action-Types";
+import {getUserId,getUserList} from "../../api/common";
+
+let actions={
+  getUserIdAPI:(phone,password,history)=>{
+    return function(dispatch,getState){
+      getUserId(phone,password).then(function(data){
+        dispatch({type:Types.GET_USERID,id:data.account.id});
+        history.push("/");
+      });
+    }
+  },
+  //获取用户歌单详情
+  getUserListAPI:(id)=>{
+    return function (dispatch,getState) {
+      dispatch({type:Types.GET_RECORD,payload:getUserList(id)});
+    }
+  }
+};
+export default actions;
--- src/store/reducer/index.js
+++ src/store/reducer/index.js
@@ -1,16 +1,13 @@
 import {combineReducers} from 'redux';
 import homeReducer from './home';
-import userDetail from "./userDetail";
-import loginReducer from "./login";
 import profileReducer from "./profile";
 import mymusicReducer from './mymusic';
 import comment from './comment';
-
+import common from './common';
 export default combineReducers({
   homeReducer,
-  userDetail,
   mymusicReducer,
   comment,
-  loginReducer,
+  common,
   profileReducer
 })
