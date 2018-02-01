86dec61e54316234e1b739c2222d3d575f1ec396
zuosuqian <614912729@qq.com>
2018-02-01 09:19:30 +0800

    左素倩提交了播放页面

--- src/containers/Detail/Detail.js
+++ src/containers/Detail/Detail.js
@@ -1,48 +1,79 @@
 import React from 'react';
 import './index.less';
+import {NavLink} from 'react-router-dom';
 
 export default class Detail extends React.Component {
     constructor() {
         super();
-        this.state = {flag: false, flag1: false, flag2: 1}
+        this.state = {
+            flag: false,
+            flag1: false,
+            flag2: 1,
+            showDownload1: {display: 'block'},
+            showDownload2: {display: 'none'},
+            appear: {display: 'none'},
+            showMusicList: {display: 'none'},
+            showListMask: {display: 'none'}
+        }
     }
 
     /*点击切换类名  //点击切换字体图标*/
     changeClass = () => {
         this.setState({flag: !this.state.flag});
     };
+
     changeName = () => {
         this.setState({flag1: !this.state.flag1});
     };
+
     changeClassName = () => {
+        this.setState({appear: {display: 'block'}});
         if (this.state.flag2 <= 3) {
             this.setState({flag2: this.state.flag2 + 1});
             if (this.state.flag2 === 3) {
                 this.setState({flag2: 1});
             }
         }
+        //=>用定时器控制切换播放方式按钮
+        this.timer=setTimeout(()=> {
+            this.setState({appear: {display: 'none'}});
+        },3000)
     };
 
+    showDownloadMask = () => {
+        this.setState({showDownload1: {display: 'block'}, showDownload2: {display: 'block'}});
+    };
+
+    hideMask = () => {
+        this.setState({showDownload1: {display: 'none'}, showDownload2: {display: 'none'}});
+    };
+
+    showMusicList=()=>{
+        this.setState({showMusicList:{display:'block'},showListMask: {display: 'block'}});
+    };
+
+    hideListMask=()=>{
+        this.setState({showListMask: {display: 'none'},showMusicList:{display:'none'}})
+    };
     render() {
         return (
             <div className="detail">
                 <div className="detailHeader">
-                    {/*<i className="back iconfont icon-fanhui"></i>*/}
-                    <i className="back iconfont">&#xe6b7;</i>
+                    <i className="back iconfont icon-fanhui"></i>
                     <div className="musicName">
                         <span className="name">无问西东</span>
-                        <span className="singer">王菲<i className="arrows iconfont icon-fanhui2"></i></span>
+                        <span className="singer">王菲</span>
                     </div>
-                    <i className="share iconfont">&#xe7c5;</i>
+                    <i className="share iconfont icon-fenxiang"></i>
                 </div>
                 <div className="detailBody">
                     <div className="btn">
                         {/*点击之前className：icon-aixin   点击之后：icon-xin*/}
                         <i className={this.state.flag ? "like iconfont icon-aixin" : "like iconfont icon-xin"}
                            onClick={this.changeClass}></i>
-                        <i className="download iconfont">&#xe64c;</i>
-                        <i className="comment iconfont">&#xe601;</i>
-                        <i className="details iconfont">&#xe60b;</i>
+                        <i className="download iconfont icon-download" onClick={this.showDownloadMask}></i>
+                        <NavLink to='/comment'><i className="comment iconfont icon-pinglun"></i></NavLink>
+                        <i className="details iconfont icon-101"></i>
                     </div>
                     <div className="progress">
                         <span className="already">00:00</span>
@@ -57,14 +88,95 @@ export default class Detail extends React.Component {
                     </div>
                 </div>
                 <div className="detailFooter">
-                    <i className={this.state.flag2 == 1 ? "goBack iconfont icon-fanhui1" : (this.state.flag2 == 2 ? "goBack iconfont icon-suijibofang" : "goBack iconfont icon-danquxunhuan")}
-                       onClick={this.changeClassName}></i>
-                    <i className="playLeft iconfont">&#xe62f;</i>
-                    <i className={this.state.flag1 ? "play iconfont icon-bofang" : "play iconfont icon-stop"}
+                    <i className={this.state.flag2 == 1 ? "goBack iconfont icon-fanhui11" : (this.state.flag2 == 2 ? "goBack iconfont icon-suijibofang" : "goBack iconfont icon-danquxunhuan")}
+                       onClick={this.changeClassName}>
+                        <span className="playMask"
+                              style={this.state.appear}>{this.state.flag2 == 1 ? "循环播放" : (this.state.flag2 == 2 ? "随机播放" : "单曲循环")}</span>
+                    </i>
+                    <i className="playLeft iconfont icon-zuobofang"></i>
+                    <i className={this.state.flag1 ? "play iconfont icon-bofang1" : "play iconfont icon-bofang11"}
                        onClick={this.changeName}></i>
-                    <i className="playRight iconfont">&#xe62e;</i>
-                    <i className="list iconfont">&#xe600;</i>
+                    <i className="playRight iconfont icon-youbofang"></i>
+                    <i className="list iconfont icon-liebiao2" onClick={this.showMusicList}></i>
                 </div>
+                <div className="downloadMask" style={this.state.showDownload1} onClick={this.hideMask}>
+                    <div className="dlMask" style={this.state.showDownload2}>
+                        <ul>
+                            <li>选择默认下载音质<span>可在账号-设置页修改</span></li>
+                            <li>标准 (128kbit/s)</li>
+                            <li>较高 (192kbit/s)</li>
+                            <li>极高 (320kbit/s)</li>
+                            <li>无损品质<i className="iconfont icon-vip1"></i></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="musicdList"  style={this.state.showMusicList}>
+                    <div className="musicdListHeader clearfix">
+                        <ul>
+                            <li>
+                                <i className="iconfont icon-suijibofang"></i>
+                            </li>
+                            <li>随机播放</li>
+                            <li>(90)</li>
+                            <li><i className="iconfont icon-dustbin_icon"></i></li>
+                            <li>收藏全部</li>
+                            <li><i className="iconfont icon-shoucang1"></i></li>
+                        </ul>
+                    </div>
+                    <ul className="musicdLists">
+                        <li>
+                            <span className="musicName">歌名歌名</span>
+                            <b className="musicSinger"> - 歌手</b>
+                            <i className="closeMusic">×</i>
+                        </li>
+                        <li>
+                            <span className="musicName">歌名歌名111</span>
+                            <b className="musicSinger"> - 歌手1111</b>
+                            <i className="closeMusic">×</i>
+                        </li>
+                        <li>
+                            <span className="musicName">歌名歌名</span>
+                            <b className="musicSinger"> - 歌手</b>
+                            <i className="closeMusic">×</i>
+                        </li>
+                        <li>
+                            <span className="musicName">歌名歌名111</span>
+                            <b className="musicSinger"> - 歌手1111</b>
+                            <i className="closeMusic">×</i>
+                        </li>
+                        <li>
+                            <span className="musicName">歌名歌名</span>
+                            <b className="musicSinger"> - 歌手</b>
+                            <i className="closeMusic">×</i>
+                        </li>
+                        <li>
+                            <span className="musicName">歌名歌名111</span>
+                            <b className="musicSinger"> - 歌手1111</b>
+                            <i className="closeMusic">×</i>
+                        </li>
+                        <li>
+                            <span className="musicName">歌名歌名</span>
+                            <b className="musicSinger"> - 歌手</b>
+                            <i className="closeMusic">×</i>
+                        </li>
+                        <li>
+                            <span className="musicName">歌名歌名111</span>
+                            <b className="musicSinger"> - 歌手1111</b>
+                            <i className="closeMusic">×</i>
+                        </li>
+                        <li>
+                            <span className="musicName">歌名歌名111</span>
+                            <b className="musicSinger"> - 歌手1111</b>
+                            <i className="closeMusic">×</i>
+                        </li>
+                        <li>
+                            <span className="musicName">歌名歌名111</span>
+                            <b className="musicSinger"> - 歌手1111</b>
+                            <i className="closeMusic">×</i>
+                        </li>
+                    </ul>
+                </div>
+                <div className="musicListMask" style={this.state.showListMask} onClick={this.hideListMask}></div>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -8,9 +8,8 @@ import MyMusic from "./containers/MyMusic/MyMusic";
 import Friend from "./containers/Friend/Friend";
 import Profile from "./containers/Profile/Profile";
 import Detail from "./containers/Detail/Detail";
+import Comment from "./containers/Detail/Comment";
 
-
-import store from './store'
 
 render(<Provider store={store}>
     <Router>
@@ -22,6 +21,8 @@ render(<Provider store={store}>
                 <Route path={'/friend'} component={Friend}/>
                 <Route path={'/profile'} component={Profile}/>
                 <Route path={'/detail/:id'} component={Detail}/>
+                <Route path={'/comment'} component={Comment}/>
+
             </Switch>
         </App>
     </Router>
@@ -32,21 +33,23 @@ import Login from "./containers/Login/Login";
 import NumLogin from "./containers/Login/NumLogin";
 import store from './store';
 import './common/index.less';
+
 render(<Provider store={store}>
-  <Router>
-    <App>
-      <Switch>
-        <Route path={'/'} exact={true} component={Home}/>
-        <Route path={'/home'} component={Home}/>
-        <Route path={'/mymusic'} component={MyMusic}/>
-        <Route path={'/friend'} component={Friend}/>
-        <Route path={'/profile'} component={Profile}/>
-        <Route path={'/detail/:id'} component={Detail}/>
-<Route path={'/login'} component={Login}/>
-<Route path={'/numlogin'} component={NumLogin}/>
-<Route path={'/detail/:id'} component={Detail}/>
-      </Switch>
-    </App>
-  </Router>
+    <Router>
+        <App>
+            <Switch>
+                <Route path={'/'} exact={true} component={Home}/>
+                <Route path={'/home'} component={Home}/>
+                <Route path={'/mymusic'} component={MyMusic}/>
+                <Route path={'/friend'} component={Friend}/>
+                <Route path={'/profile'} component={Profile}/>
+                <Route path={'/detail/:id'} component={Detail}/>
+                <Route path={'/login'} component={Login}/>
+                <Route path={'/numlogin'} component={NumLogin}/>
+                <Route path={'/detail/:id'} component={Detail}/>
+                <Route path={'/comment'} component={Comment}/>
+            </Switch>
+        </App>
+    </Router>
 </Provider>, window.root);
 
